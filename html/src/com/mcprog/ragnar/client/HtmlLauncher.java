984c39be6ff1209e2bf84b7bfd3d3761031019cd
mcprog <michaelcurtis96@gmail.com>
2015-01-30 22:42:06 -0800

    html launcher

---
+++ html/src/com/mcprog/ragnar/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.mcprog.ragnar.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mcprog.ragnar.Ragnar;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(1024, 640);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Ragnar();
+        }
+}
