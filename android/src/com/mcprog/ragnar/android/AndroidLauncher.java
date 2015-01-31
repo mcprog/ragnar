a56b212f97a3c999e4c004719093e9bd7158169f
mcprog <michaelcurtis96@gmail.com>
2015-01-30 22:31:05 -0800

    androd launcher

---
+++ android/src/com/mcprog/ragnar/android/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.mcprog.ragnar.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mcprog.ragnar.Ragnar;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useImmersiveMode = true;
+		initialize(new Ragnar(), config);
+	}
+}
