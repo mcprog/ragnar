888530edebba5152a937fd5ace89761dc3f91661
mcprog <michaelcurtis96@gmail.com>
2015-02-27 23:06:16 -0800

    fixed bug with loading, but pause menu needs fixking
    Signed-off-by: mcprog <michaelcurtis96@gmail.com>

--- core/src/com/mcprog/ragnar/screens/LoadingScreen.java
+++ core/src/com/mcprog/ragnar/screens/LoadingScreen.java
@@ -1,17 +1,14 @@
 package com.mcprog.ragnar.screens;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mcprog.ragnar.Ragnar;
 import com.mcprog.ragnar.lib.Assets;
 import com.mcprog.ragnar.lib.RagnarConfig;
 
 public class LoadingScreen extends ScreenDrawable {
 	
+	private boolean setup = false;
 	
 	public LoadingScreen(Ragnar game) {
 		super(game);
@@ -29,24 +26,7 @@ public class LoadingScreen extends ScreenDrawable {
 			fontBatch.begin();
 			Assets.ragnarFont.draw(fontBatch, "Assets loaded", -fontCamera.viewportWidth / 4,  Assets.ragnarFont.getCapHeight() / 2);
 			fontBatch.end();
-			/*
-			 * Load mutations and assign vars
-			 */
-			Assets.playerAnimations = Assets.loadAnimation(Assets.PLAYER_PATH);
-			Assets.playerGirlAnimations = Assets.loadAnimation(Assets.PLAYER_GIRL_PATH);
-			Assets.assignLoadedAssets();
-			/*
-			 * instantiate screens
-			 */
-			RagnarConfig.init();
-			Assets.testLoadI18N();
-			game.gameScreen = new GameScreen(game);
-			game.killScreen = new KillScreen(game);
-			game.winScreen = new WinScreen(game);
-			game.settingsScreen = new SettingsScreen(game);
-			game.menuScreen = new MenuScreen(game);
-			game.highscoreScreen = new HighscoreScreen(game);
-			game.creditsScreen = new CreditsScreen(game);
+			setupAfterLoadOnce();
 			if (Gdx.input.justTouched()) {
 				game.setScreen(game.menuScreen);
 			}
@@ -58,6 +38,32 @@ public class LoadingScreen extends ScreenDrawable {
 			Assets.ragnarFont.draw(fontBatch, "Assets loading ... " + (int)Assets.getLoadingProgress() + "%", -fontCamera.viewportWidth / 4, Assets.ragnarFont.getCapHeight() / 2);
 			fontBatch.end();
 		}
+	}
+	
+	private void setupAfterLoadOnce () {
+		if (setup) {
+			return;
+		}
+		/*
+		 * Load mutations and assign vars
+		 */
+		Assets.playerAnimations = Assets.loadAnimation(Assets.PLAYER_PATH);
+		Assets.playerGirlAnimations = Assets.loadAnimation(Assets.PLAYER_GIRL_PATH);
+		Assets.assignLoadedAssets();
+		/*
+		 * instantiate screens
+		 */
+		RagnarConfig.init();
+		Assets.testLoadI18N();
+		game.gameScreen = new GameScreen(game);
+		game.killScreen = new KillScreen(game);
+		game.winScreen = new WinScreen(game);
+		game.settingsScreen = new SettingsScreen(game);
+		game.menuScreen = new MenuScreen(game);
+		game.highscoreScreen = new HighscoreScreen(game);
+		game.creditsScreen = new CreditsScreen(game);
+		
+		setup = true;
 	}
 
 }
